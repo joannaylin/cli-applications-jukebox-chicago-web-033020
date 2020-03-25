c6d4cb5057c605c1288f7d0e9c17cb46969f7eba
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-25 19:03:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,6 +25,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   response = gets.strip
   songs.each do |listing|
+    binding.pry
     if response == listing
       puts "Playing #{listing}"
     else
